a8fbaa39e1627f2bcfaf3dabdba33809356bce75
Nguyễn Hữu Niên <125124829+NguyenHuuNien@users.noreply.github.com>
2024-01-04 16:03:29 +0700

    fix all

--- Assets/Scripts/ButtonGround.cs
+++ Assets/Scripts/ButtonGround.cs
@@ -6,6 +6,12 @@ public class ButtonGround : MonoBehaviour
 {
     [Header("Buttons of Menu")]
     [SerializeField] private GameObject[] buttons_menu;
+    [Header("Buttons of Animals")]
+    [SerializeField] private GameObject[] buttons_animal;
+    [Header("Buttons of Trees")]
+    [SerializeField] private GameObject[] buttons_tree;
+    [Header("Buttons of Events")]
+    [SerializeField] private GameObject[] buttons_event;
     private bool isOpenMenu = false;
     private void changeStateMenu(){
         foreach(var o in buttons_menu){
