993ff0042b3a355e069204d823069d109432e753
Nguyễn Hữu Niên <125124829+NguyenHuuNien@users.noreply.github.com>
2024-01-07 01:07:55 +0700

    add UI

--- Assets/Scripts/SetTimeUpdate.cs
+++ Assets/Scripts/SetTimeUpdate.cs
@@ -7,21 +7,17 @@ using UnityEngine.UI;
 public class SetTimeUpdate : MonoBehaviour
 {
     [SerializeField] private Text txtTimeWorld;
-    [SerializeField] private Text txtTimeSave;
-    private void Start() {
-        TimeWorld tw = new TimeWorld();
-        tw = SaveLoadData.LoadData();
-        txtTimeSave.text = tw.Gio + ":" + tw.Phut + ":" + tw.Giay;
-    }
     private void Update() {
-        txtTimeWorld.text = DateTime.Now.Hour + ":" + 
-        DateTime.Now.Minute + ":" + DateTime.Now.Second;
-    }
-    private void OnDisable() {
-        TimeWorld tw = new TimeWorld();
-        tw.Gio = DateTime.Now.Hour;
-        tw.Phut = DateTime.Now.Minute;
-        tw.Giay = DateTime.Now.Second;
-        SaveLoadData.SaveData(tw);
+        string hour = DateTime.Now.Hour.ToString().Length == 1 ? 
+                        "0"+DateTime.Now.Hour.ToString() : 
+                        DateTime.Now.Hour.ToString();
+        string minute = DateTime.Now.Minute.ToString().Length == 1 ? 
+                        "0"+DateTime.Now.Minute.ToString() : 
+                        DateTime.Now.Minute.ToString();
+        string second = DateTime.Now.Second.ToString().Length == 1 ? 
+                        "0"+DateTime.Now.Second.ToString() : 
+                        DateTime.Now.Second.ToString();
+                        
+        txtTimeWorld.text = hour + ":" + minute + ":" + second;
     }
 }
